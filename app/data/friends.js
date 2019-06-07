9e24389d2ff9a33a9ca77f50d569219fb1fa0879
Jason Arnold <jmarn94@gmail.com>
2019-06-06 23:17:25 -0500

    updated files, still throwing errors

---
+++ app/data/friends.js
@@ -0,0 +1,163 @@
+var friendsArray = [
+	{
+		name: 'Tony',
+		photo: 'https://media.newyorker.com/photos/5c4b9c1e4dc6e2423506f36a/master/w_727,c_limit/Helfand-TonyRomo-2.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Troy',
+		photo: 'https://foxsports-wordpress-www-prsupports-prod.s3.amazonaws.com/uploads/sites/2/2016/12/PHOTO-Aikman-HS-727x727-480x480.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Emmit',
+		photo: 'https://cdn.shopify.com/s/files/1/0589/1069/products/Cowboys_Emmitt_Smith_-_AATF186_800x.jpg?v=1541988811',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Zeke',
+		photo: 'https://s.yimg.com/ny/api/res/1.2/XgifhXn3p1DGJ54D9CkTcg--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/http://media.zenfs.com/en-US/homerun/uk.sporting.news/0ea67b621a2ceae79911358f9afda0fb',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Dez',
+		photo: 'https://clutchpoints.com/wp-content/uploads/2019/06/Dez_Bryant.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Dak',
+		photo: 'https://images2.minutemediacdn.com/image/upload/c_fill,w_912,h_516,f_auto,q_auto,g_auto/shape/cover/sport/Divisional-Round---Dallas-Cowboys-v-Los-Angeles-Rams-9996ffdcc83f2002701317cfcbdf4b31.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Michael',
+		photo: 'https://s.yimg.com/ny/api/res/1.2/hG4N8x8oHuGKEIkCQ8cZZQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/http://media.zenfs.com/en-US/homerun/people_218/e4edd544419e94614ae9c0d23b35ff5f',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Amari',
+		photo: 'https://www.washingtonpost.com/resizer/g1BqfmpfN1qh47BcXJgzclCQlY4=/1484x0/arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/FRVI5VX4DUI6RIL6CYVXCLUPYI.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Leighton',
+		photo: 'https://usatcowboyswire.files.wordpress.com/2018/12/usatsi_11819605.jpg?w=1000&h=600&crop=1',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Jaylon',
+		photo: 'https://cdn2.newsok.biz/cache/large960_blur-42cacc31832f8508c512390d756a3de7.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+module.exports = friendsArray;
